018e3a59f9a5b40d597c5dfb6aebdd6da4b1dcca
Tunous <lukus178@gmail.com>
2017-08-10 21:53:47 +0200

    Add experimental commit header layout

--- app/src/main/java/me/thanel/gitlog/base/BaseActivity.kt
+++ app/src/main/java/me/thanel/gitlog/base/BaseActivity.kt
@@ -5,13 +5,16 @@ import android.arch.lifecycle.LifecycleRegistryOwner
 import android.os.Bundle
 import android.os.Parcelable
 import android.support.annotation.LayoutRes
+import android.support.design.widget.AppBarLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.View
 import me.thanel.gitlog.R
 
 abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
 
     private val lifecycleRegistry by lazy { LifecycleRegistry(this) }
+    private val appBarLayout by lazy { findViewById(R.id.appBarLayout) as AppBarLayout }
 
     protected open val title: String? = null
 
@@ -40,6 +43,13 @@ abstract class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
     }
 
     override fun getLifecycle() = lifecycleRegistry
+
+    fun addViewAboveToolbar(view: View) {
+        appBarLayout.addView(view, appBarLayout.childCount - 1, AppBarLayout.LayoutParams(
+                AppBarLayout.LayoutParams.MATCH_PARENT,
+                AppBarLayout.LayoutParams.WRAP_CONTENT
+        ))
+    }
 
     protected fun <T : Parcelable> parcelableExtra(name: String) = lazy {
         intent.getParcelableExtra<T>(name)
--- app/src/main/java/me/thanel/gitlog/commit/CommitActivity.kt
+++ app/src/main/java/me/thanel/gitlog/commit/CommitActivity.kt
@@ -1,15 +1,22 @@
 package me.thanel.gitlog.commit
 
-import android.arch.lifecycle.Observer
 import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import me.thanel.gitlog.R
 import me.thanel.gitlog.base.BasePagerActivity
 import me.thanel.gitlog.repository.RepositoryActivity
 import me.thanel.gitlog.utils.createIntent
+import me.thanel.gitlog.utils.observe
+import me.thanel.gitlog.view.AvatarDrawable
 
 class CommitActivity : BasePagerActivity() {
     private val commitSha by stringExtra(EXTRA_COMMIT_SHA)
     private val repositoryId by intExtra(EXTRA_REPOSITORY_ID)
+
+    private lateinit var headerView: View
 
     override val title: String?
         get() = "Commit ${commitSha.substring(0, 7)}"
@@ -20,11 +27,27 @@ class CommitActivity : BasePagerActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val viewModel = CommitViewModel.get(this, repositoryId, commitSha)
+        headerView = layoutInflater.inflate(R.layout.view_commit_header, null)
+        addViewAboveToolbar(headerView)
 
-        viewModel.repository.observe(this, Observer {
+        val viewModel = CommitViewModel.get(this, repositoryId, commitSha)
+        observeViewModel(viewModel)
+    }
+
+    private fun observeViewModel(viewModel: CommitViewModel) {
+        viewModel.repository.observe(this) {
             subtitle = it?.name
-        })
+            val repositoryNameView = headerView.findViewById(R.id.repositoryNameView) as TextView
+            repositoryNameView.text = it?.name
+        }
+        viewModel.commit.observe(this) {
+            val titleView = headerView.findViewById(R.id.titleView) as TextView
+            titleView.text = it?.shortMessage
+
+            val authorName = it?.authorIdent?.name ?: ""
+            val commitAuthorAvatarView = headerView.findViewById(R.id.commitAuthorAvatarView) as ImageView
+            commitAuthorAvatarView.setImageDrawable(AvatarDrawable(authorName, commitSha))
+        }
     }
 
     override fun createFragment(position: Int) = when (position) {
