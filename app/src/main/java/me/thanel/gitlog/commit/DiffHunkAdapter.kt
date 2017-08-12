52e5097685d2dbb33a329e9d7dfe5ad0cb4a6410
Tunous <lukus178@gmail.com>
2017-08-12 14:53:24 +0200

    Make diff hunks collapsible

--- app/src/main/java/me/thanel/gitlog/commit/DiffHunkAdapter.kt
+++ app/src/main/java/me/thanel/gitlog/commit/DiffHunkAdapter.kt
@@ -1,6 +1,8 @@
 package me.thanel.gitlog.commit
 
+import android.graphics.Color
 import android.view.View
+import android.widget.TextView
 import me.thanel.gitlog.R
 import me.thanel.gitlog.base.ItemAdapter
 import me.thanel.gitlog.view.DiffHunkView
@@ -17,12 +19,36 @@ class DiffHunkAdapter(
             itemView: View,
             private val viewModel: CommitViewModel
     ) : ItemAdapter.ViewHolder<DiffEntry>(itemView) {
+        private val fileNameView by lazy { itemView.findViewById<TextView>(R.id.fileNameView) }
         private val diffHunkView by lazy { itemView.findViewById<DiffHunkView>(R.id.diffHunkView) }
+
+        init {
+            fileNameView.setOnClickListener {
+                diffHunkView.visibility = if (diffHunkView.visibility == View.VISIBLE) View.GONE
+                else View.VISIBLE
+            }
+        }
 
         override fun bind(item: DiffEntry) {
             super.bind(item)
 
+            fileNameView.text = when (item.changeType) {
+                DiffEntry.ChangeType.ADD -> item.newPath
+                DiffEntry.ChangeType.RENAME,
+                DiffEntry.ChangeType.COPY -> "${item.oldPath} -> ${item.newPath}"
+                else -> item.oldPath
+            }
+
+            val color = when (item.changeType) {
+                DiffEntry.ChangeType.ADD -> Color.GREEN
+                DiffEntry.ChangeType.DELETE -> Color.RED
+                DiffEntry.ChangeType.RENAME,
+                DiffEntry.ChangeType.COPY -> Color.BLUE
+                else -> Color.BLACK
+            }
+            fileNameView.setTextColor(color)
             diffHunkView.setDiff(viewModel.formatDiffEntry(item))
+            diffHunkView.visibility = View.GONE
         }
     }
 }
