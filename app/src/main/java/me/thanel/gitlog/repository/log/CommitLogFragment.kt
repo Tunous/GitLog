274ce7066971a3a5d65d68d5fd1c5e4aa73dae88
Tunous <lukus178@gmail.com>
2017-08-12 20:54:00 +0200

    Add branch listing and checkout

--- app/src/main/java/me/thanel/gitlog/repository/log/CommitLogFragment.kt
+++ app/src/main/java/me/thanel/gitlog/repository/log/CommitLogFragment.kt
@@ -1,61 +1,63 @@
 package me.thanel.gitlog.repository.log
 
-import android.app.Application
-import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.LiveData
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.widget.LinearLayoutManager
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_commit_log.*
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.run
-import me.thanel.gitlog.GitLogApplication
 import me.thanel.gitlog.R
 import me.thanel.gitlog.base.BaseFragment
 import me.thanel.gitlog.commit.CommitActivity
 import me.thanel.gitlog.db.model.Repository
-import me.thanel.gitlog.utils.getViewModel
 import me.thanel.gitlog.utils.observe
 import me.thanel.gitlog.utils.withArguments
-import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.ListBranchCommand
+import org.eclipse.jgit.lib.Ref
 import org.eclipse.jgit.revwalk.RevCommit
-import java.io.File
 
 class CommitLogFragment : BaseFragment<CommitLogViewModel>() {
     private val repositoryId by intArg(ARG_REPOSITORY_ID)
-
-    private val adapter = CommitLogAdapter(this::openCommit)
+    private val commitLogAdapter = CommitLogAdapter(this::openCommit)
+    private val branchListAdapter = BranchListAdapter(this::checkout)
 
     private lateinit var repository: Repository
-
-    private lateinit var git: Git
-    private lateinit var repositoryFile: File
+    private lateinit var branchesBottomSheetBehavior: BottomSheetBehavior<ViewGroup>
 
     override val layoutResId: Int
         get() = R.layout.fragment_commit_log
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        commitLogRecycler.adapter = adapter
-        commitLogRecycler.layoutManager = LinearLayoutManager(context)
-    }
-
     override fun onCreateViewModel() = CommitLogViewModel.get(activity, repositoryId)
 
     override fun observeViewModel(viewModel: CommitLogViewModel) {
-        viewModel.repository.observe(this, this::displayLog)
+        viewModel.repository.observe(this, this::onRepositoryLoaded)
     }
 
-    private fun displayLog(it: Repository?) {
-        if (it != null) {
-            repository = it
-            repositoryFile = File(context.filesDir, "repos/${it.name}")
-            git = Git.open(repositoryFile)
-            logCommits()
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        commitLogRecycler.apply {
+            adapter = commitLogAdapter
+            layoutManager = LinearLayoutManager(context)
         }
+        branchListRecyclerView.apply {
+            adapter = branchListAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+        branchesBottomSheetBehavior = BottomSheetBehavior.from(branchListBottomSheet)
+    }
+
+    private fun onRepositoryLoaded(it: Repository?) {
+        if (it == null) {
+            // TODO: Loading
+            return
+        }
+        repository = it
+        logCommits()
+        listBranches()
     }
 
     private fun openCommit(commit: RevCommit) {
@@ -64,8 +66,32 @@ class CommitLogFragment : BaseFragment<CommitLogViewModel>() {
     }
 
     private fun logCommits() = launch(UI) {
-        val log = run(CommonPool) { git.log().call() }
-        adapter.addAll(log)
+        val log = run(CommonPool) {
+            repository.git.log().call()
+        }
+        commitLogAdapter.replaceAll(log)
+    }
+
+    private fun listBranches() = launch(UI) {
+        val branches = run(CommonPool) {
+            repository.git.branchList()
+                    .setListMode(ListBranchCommand.ListMode.ALL)
+                    .call()
+        }
+        branchListAdapter.addAll(branches)
+    }
+
+    private fun checkout(ref: Ref) {
+        currentBranchTextView.text = ref.name
+        branchesBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        launch(UI) {
+            run(CommonPool) {
+                repository.git.checkout()
+                        .setName(ref.name)
+                        .call()
+            }
+            logCommits()
+        }
     }
 
     companion object {
@@ -73,22 +99,6 @@ class CommitLogFragment : BaseFragment<CommitLogViewModel>() {
 
         fun newInstance(repositoryId: Int) = CommitLogFragment().withArguments {
             putInt(ARG_REPOSITORY_ID, repositoryId)
-        }
-    }
-}
-
-class CommitLogViewModel(
-        application: Application,
-        private val repositoryId: Int
-) : AndroidViewModel(application) {
-    private val db = (application as GitLogApplication).database
-
-    val repository: LiveData<Repository>
-        get() = db.repositoryDao().getRepository(repositoryId)
-
-    companion object {
-        fun get(activity: FragmentActivity, repositoryId: Int) = getViewModel(activity) {
-            CommitLogViewModel(activity.application, repositoryId)
         }
     }
 }
