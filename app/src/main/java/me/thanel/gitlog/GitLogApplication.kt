66ff9276b423cea4b37f69637e7959d53173bb68
Tunous <lukus178@gmail.com>
2017-08-17 14:53:29 +0200

    Use OkHttpDownloader for picasso

--- app/src/main/java/me/thanel/gitlog/GitLogApplication.kt
+++ app/src/main/java/me/thanel/gitlog/GitLogApplication.kt
@@ -3,6 +3,8 @@ package me.thanel.gitlog
 import android.app.Application
 import android.arch.persistence.room.Room
 import com.chibatching.kotpref.Kotpref
+import com.jakewharton.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
 import me.thanel.gitlog.db.Database
 
 class GitLogApplication : Application() {
@@ -14,5 +16,10 @@ class GitLogApplication : Application() {
 
         database = Room.databaseBuilder(this, Database::class.java, "gitlog.db").build()
         Kotpref.init(this)
+
+        val picasso = Picasso.Builder(this)
+                .downloader(OkHttp3Downloader(this, Integer.MAX_VALUE.toLong()))
+                .build()
+        Picasso.setSingletonInstance(picasso)
     }
 }
