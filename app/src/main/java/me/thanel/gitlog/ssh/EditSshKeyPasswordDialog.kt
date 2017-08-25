4213e40c8539660ce52db5a67cd1d4d64491c424
Łukasz Rutkowski <lukus178@gmail.com>
2017-08-25 19:25:22 +0200

    Pre-fill edit password dialog with current password

--- app/src/main/java/me/thanel/gitlog/ssh/EditSshKeyPasswordDialog.kt
+++ app/src/main/java/me/thanel/gitlog/ssh/EditSshKeyPasswordDialog.kt
@@ -1,17 +1,26 @@
 package me.thanel.gitlog.ssh
 
+import android.content.SharedPreferences
+import android.os.Bundle
 import me.thanel.gitlog.R
 import me.thanel.gitlog.base.dialog.InputDialog
 import me.thanel.gitlog.utils.stringArg
 import me.thanel.gitlog.utils.withArguments
 
 class EditSshKeyPasswordDialog : InputDialog() {
+    private lateinit var prefs: SharedPreferences
     private val keyName by stringArg(ARG_KEY_NAME)
 
     override val titleResId get() = R.string.edit_password
     override val hintResId get() = R.string.password
     override val isPasswordInput get() = true
     override val positiveButtonResId get() = R.string.edit
+    override val inputText: String? get() = prefs.getString(keyName, null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        prefs = SharedPreferencesCredentialsProvider.getPrefs(context)
+    }
 
     override fun onSubmit(input: String): Boolean {
         SharedPreferencesCredentialsProvider.getPrefs(context)
